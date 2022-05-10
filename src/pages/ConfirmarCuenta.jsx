f616e1885f4198f99a72e92e79102a0ef909798f
pipeswk <afco1995@gmail.com>
2022-05-09 23:18:19 -0500

    Pagina ConfirmarCuenta v1

---
+++ src/pages/ConfirmarCuenta.jsx
@@ -0,0 +1,13 @@
+
+
+const ConfirmarCuenta = () => {
+  return (
+    <>
+      <h1 className='text-sky-600 font-black md:text-6xl text-4xl capitalize text-center'>
+        Tu cuenta ha sido <span className='text-slate-700'>Confirmada</span>
+      </h1>
+    </>
+  )
+}
+
+export default ConfirmarCuenta
