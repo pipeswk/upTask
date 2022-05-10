90fe84d809f0222e26689173dee47c55ece7d448
pipeswk <afco1995@gmail.com>
2022-05-09 23:17:17 -0500

    Se agregan paginas publicas al router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Routes,   Route } from 'react-router-dom';
 import AuthLayout from './layouts/AuthLayout';
+import ConfirmarCuenta from './pages/ConfirmarCuenta';
+import Login from './pages/Login';
+import NuevoPassword from './pages/NuevoPassword';
+import OlvidePassword from './pages/OlvidePassword';
+import Registrar from './pages/Registrar';
+
 
 function App() {
 
@@ -7,7 +13,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<AuthLayout />}> 
-
+          <Route index element={<Login />} />
+          <Route path='registrar' element={<Registrar />} />
+          <Route path='olvide-password' element={<OlvidePassword />} />
+          <Route path='olvide-password/:token' element={<NuevoPassword />} />
+          <Route path='confirmar-cuenta/:id' element={<ConfirmarCuenta />} />
         </Route>
       </Routes>
     </BrowserRouter>
