e8142d6ca240d932a67b15257ce5fd497981ae21
pipeswk <afco1995@gmail.com>
2022-05-09 23:18:04 -0500

    Layout publico v1

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,8 +1,14 @@
-
+import { Outlet } from 'react-router-dom'
 
 const AuthLayout = () => {
   return (
-    <div>AuthLayout</div>
+    <>
+      <main className='container mx-auto mt-5 md:mt-5 p-5 md:flex md:justify-center'>
+        <div className='md:w-2/3 lg:w-1/3'>
+          <Outlet />
+        </div>
+      </main>
+    </>
   )
 }
 
